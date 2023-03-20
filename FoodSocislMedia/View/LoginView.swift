dc2975c342b55898c1ac8dcae546b5d512a2bf8d
Abdelrahman Abo Al Nasr <abdelrahman_adm@outlook.com>
2023-03-20 17:09:39 +0200

    Initial Commit

---
+++ FoodSocislMedia/View/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  FoodSocislMedia
+//
+//  Created by Abdelrahman Abo Al Nasr on 20/03/2023.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}

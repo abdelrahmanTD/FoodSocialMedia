f220725f47b7ad28b98d123278e861bcceb1161b
Abdelrahman Abo Al Nasr <abdelrahman_adm@outlook.com>
2023-03-24 01:11:38 +0200

    Add request errors types

---
+++ FoodSocislMedia/Networking/RequestError.swift
@@ -0,0 +1,28 @@
+//
+//  RequestError.swift
+//  FoodSocislMedia
+//
+//  Created by Abdelrahman Abo Al Nasr on 24/03/2023.
+//
+
+import Foundation
+
+enum RequestError: Error {
+	case invalidURL
+	case noResponse
+	case invalidResponse
+	case invalidDecoding
+	case unknownError
+}
+
+extension RequestError {
+	public var errorMessage: String {
+		switch self {
+			case .invalidURL: return "Request URL is not correct."
+			case .noResponse: return "No response from the server."
+			case .invalidResponse: return "Response is not valid."
+			case .invalidDecoding: return "Couldn't decode response."
+			case .unknownError: return "Unknown error occurred."
+		}
+	}
+}

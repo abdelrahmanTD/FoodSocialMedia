9430f89984907210a49b142ba01a33c11f0a0136
Abdelrahman Abo Al Nasr <abdelrahman_adm@outlook.com>
2023-03-25 16:00:02 +0200

    Add the design of the custom tab bar icon

---
+++ FoodSocislMedia/CustomView/CustomTabBar/TabBarButton.swift
@@ -0,0 +1,50 @@
+//
+//  TabBarButton.swift
+//  FoodSocialMedia
+//
+//  Created by Abdelrahman Abo Al Nasr on 25/03/2023.
+//
+
+import SwiftUI
+
+struct TabBarButton: View {
+	let label: Tabs
+	@Binding var selectedTab: Tabs
+
+	var animation: Namespace.ID
+
+    var body: some View {
+        Button(action: {
+			withAnimation {
+				self.selectedTab = self.label
+			}
+		}) {
+			VStack {
+				// MARK: - Top Indicator
+				ZStack {
+					TabBarIndicator()
+						.fill(.clear)
+
+					if selectedTab == label {
+						TabBarIndicator()
+							.fill(Color.primaryColor)
+							.matchedGeometryEffect(id: "tab_change", in: animation)
+					}
+				}
+				.frame(width: 25, height: 5)
+
+				// MARK: - Tab Icon
+				Image(label.rawValue)
+					.foregroundColor(selectedTab == label ? .primaryColor : .iconColor)
+			}
+		}
+    }
+}
+
+struct TabBarButton_Previews: PreviewProvider {
+	@Namespace static var animation
+
+    static var previews: some View {
+		TabBarButton(label: .home, selectedTab: .constant(.home), animation: animation)
+    }
+}

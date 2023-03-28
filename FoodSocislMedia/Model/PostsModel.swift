f15598669bbfc413661183973fa4dec990aa753d
Abdelrahman Abo Al Nasr <abdelrahman_adm@outlook.com>
2023-03-28 07:03:48 +0200

    Add post model

---
+++ FoodSocislMedia/Model/PostsModel.swift
@@ -0,0 +1,41 @@
+//
+//  PostsModel.swift
+//  FoodSocialMedia
+//
+//  Created by Abdelrahman Abo Al Nasr on 27/03/2023.
+//
+
+import Foundation
+
+struct Posts: Decodable {
+	let posts: [Post]
+	let limit: Int
+}
+
+struct Post: Identifiable, Decodable {
+	let id: Int
+	let body: String
+	let userId: Int
+	
+	var user: User?
+}
+
+extension Post {
+	var publishedSince: String {
+		// Random number of seconds - max 2 days.
+		let timeInSeconds = Int.random(in: 1 ... 172_800)
+
+		// Create a date from the number of seconds that generated.
+		let calendar = Calendar.current
+		let date = calendar.date(byAdding: .second, value: -timeInSeconds, to: Date())
+
+		// Calculate the difference between current date and the published date
+		let timeInterval = Date().timeIntervalSince(date!)
+
+		// Format the calculated date and convert it to String
+		let formattedString = Formatter.dateFormatter.string(from: timeInterval) ?? "0 sec"
+
+		// Return formatted date
+		return "\(formattedString) ago"
+	}
+}
